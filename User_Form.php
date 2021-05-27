4fde6dcd564a755f3c6a80ab3d2c6de5eaf5a79b
Himu143 <78273243+Himu143@users.noreply.github.com>
2021-05-27 14:13:39 +0600

    first commit

---
+++ User_Form.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        
+</head>
+        <body>
+                <title>My Home Page</title>
+
+                <h1>User Registration</h1>
+                <h2>Fill up the form </h2>
+
+<form>
+    <table>
+        <tr>
+            <td>
+                First Name: 
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>  
+        <tr>
+            <td>
+                Last Name: 
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Password: 
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        <tr>
+            <td>
+                Confirm Password:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Email:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Blood group:
+            </td>
+            <td>
+                <select>  
+                    <option>A+</option>
+                    <option>A-</option>
+                    <option>B+</option>
+                    <option>B-</option>
+                    <option>AB+</option>
+                    <option>AB-</option>
+                    <option>O+</option>
+                    <option>O-</option>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Date of Birth:
+            </td>
+            <td>
+                <input type="date">
+            </td>
+        </tr>
+        <tr>
+              <td>  
+                    Choose an option 
+                </td>
+        </tr>
+        <tr>
+                <td>
+                        Male <input type="radio" name= "gender"> 
+                </td>
+        
+        
+                <td>
+                        Female <input type="radio" name="gender"> 
+                </td>
+        </tr>
+
+             <tr>
+        
+                <td> 
+                     <br>     <input type="submit" value="Register">
+                </td>
+                <td>
+                      <br>  <input type="reset">
+                </td>
+            </tr>
+    </table>
+        </form>
+
+    </body>
+</html>
