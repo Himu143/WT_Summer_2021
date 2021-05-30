640f4f402dbe07222dae95f5a1641859784b156f
Himu143 <78273243+Himu143@users.noreply.github.com>
2021-05-30 17:36:55 +0600

    Add files via upload
    first commit

--- LabTask1/registration.php
+++ LabTask1/registration.php
@@ -1,1 +1,101 @@
+<!DOCTYPE html>
+<html>
+    <head>
 
+        <title>Registration Form</title>
+
+</head>
+        <body>
+
+                <h1><u> Registration Form</u></h1>
+<form>
+    <table>
+        <tr>
+            <td>
+                Full Name:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>  
+        <tr>
+            <td>
+                Email: 
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Password: 
+            </td>
+            <td>
+                <input type="password"  name="pwd">
+            </td>
+        <tr>
+            
+           
+        </tr>
+        <td>
+                Comment:
+            </td>
+            <td>
+                <textarea name="" id="" cols="30" rows="4">
+                </textarea>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+             
+        </tr>
+       
+        <tr>
+              <td> 
+                   Gender: 
+              </td>
+               <td>   
+                        <input type="radio" name= "gender">Female
+                        <input type="radio" name="gender">Male
+                        <input type="radio" name="gender">Other
+               </td>
+               
+        </tr>
+        <tr>
+              <td>  
+                   Please choose a hobby:
+                </td>
+                <td> 
+                        <input type="radio" name= "hobby">Singing
+                        <input type="radio" name="hobby">Dancing
+                        <input type="radio" name="hobby">Reading
+                </td>
+                
+        </tr>
+        <tr>
+                <td> 
+                    <label for="myfile">Please Choose a File:</label>
+               </td>
+                
+                <td>
+                     <input type="file" id="myfile" name="myfile"> 
+                
+                </td>
+        
+    
+        </tr>
+        
+             <tr>
+        
+                <td> 
+                     <br>   <input type="submit" value="Submit">
+                           <input type="reset">
+                </td>
+           
+ 
+         </tr>
+    </table>
+        </form>
+
+    </body>
+</html>
